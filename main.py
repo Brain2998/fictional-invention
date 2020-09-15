cbeb588caecada4819ef29215a22faf3b87877f8
Brain2998 <an.sereb96@gmail.com>
2020-09-16 01:50:02 +0300

    connect to the database

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 import flask
+import configparser
 from static import static
 from session import session
 import pymysql
+import os
+import sys
 
 app = flask.Flask(__name__)
 
 if __name__ == "__main__":
-    db=pymysql.connect
+    parser=configparser.ConfigParser()
+    parser.read_file(open('config.ini', 'r'))
+    appHost=parser.get('Common', 'APP_ADDRESS')
+    appPort=parser.get('Common', 'APP_PORT')
+    dbAddr=parser.get('DB', 'DB_ADDRESS')
+    dbPort=parser.get('DB', 'DB_PORT')
+
+    dbLogin=''
+    dbPassword=''
+    try:
+        dbLogin=os.environ['DatabaseLogin']
+        dbPassword=os.environ['DatabasePassword']
+    except KeyError:
+        print('Please define environment variable DatabaseLogin and DatabasePassword')
+        sys.exit(1)
+    db=pymysql.connect(dbAddr, int(dbPort), dbLogin, dbPassword, 'fictionalinvention')
+
     app.register_blueprint(static)
     app.register_blueprint(session)
-    app.run(host="0.0.0.0", port=80)
+    app.run(host=appHost, port=int(appPort))
